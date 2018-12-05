fd5f677972ce1b856096bcce4bd12d644dc140c2
cherrus <ruslan.chertenkov@gmail.com>
2018-12-05 15:26:02 +0300

    Lesson 2. Add lots

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ $user_avatar = 'img/user.jpg';
 </header>
 <?php
     $categories = ["Доски и лыжи","Крепления","Ботинки","Одежда","Инструменты", "Разное"];
-    $advt_list = [
+    $lots_list = [
             [
                 "name" => "2014 Rossignol District Snowboard",
                 "category" => "Доски и лыжи",
@@ -119,17 +119,19 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+            <?php $index = 0; ?>
+            <?php while ($index < count($lots_list)): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src=<?=$lots_list[$index]["image_url"];?> width="350" height="260" alt=<?=$lots_list[$index]["category"];?>>
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$lots_list[$index]["category"];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lots_list[$index]["name"];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$lots_list[$index]["price"];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -137,6 +139,8 @@ $user_avatar = 'img/user.jpg';
                     </div>
                 </div>
             </li>
+            <?php $index++; ?>
+            <?php endwhile; ?>
         </ul>
     </section>
 </main>
