ef8cf76b90f2f8f23d5e56a30f2c317dfcf5a366
cherrus <ruslan.chertenkov@gmail.com>
2018-12-08 12:04:21 +0300

    Lesson 3. Add templates separation

---
+++ functions.php
@@ -0,0 +1,37 @@
+<?php
+function include_template($template, $data){
+    //ini_set("zlib.output_compression", 0);
+    if (file_exists($template)){
+
+        foreach($data as $key => $value){
+            $key_clone = &$key;
+            $name = $key_clone;
+            $$name = $value;
+        }
+        //indian shit code
+        /*if(isset($data["content"]))
+            $content = $data["content"];
+        if(isset($data["title"]))
+            $title = $data["title"];
+        if(isset($data["lots_list"]))
+            $lots_list = $data["lots_list"];
+        if(isset($data["categories"]))
+            $categories = $data["categories"];
+        ..etc
+        */
+        //or like switch
+        ob_start();
+        require_once($template);
+        return ob_get_clean();
+    }
+    else
+       return "ebi koney suka";
+}
+function price_format($price){
+    $result = ceil($price);
+    if ($result >= 1000){
+        return number_format($result, 0, "", " ") . " &#8381";
+    }
+    else
+        return $result . "&#8381" ;
+}
---
+++ templates/index.php
@@ -0,0 +1,43 @@
+
+<section class="promo">
+    <h2 class="promo__title">Нужен стафф для катки?</h2>
+    <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+    <ul class="promo__list">
+        <?php $index = 0; ?>
+        <?php while ($index < count($categories)): ?>
+            <li class="promo__item promo__item--boards">
+                <a class="promo__link" href="all-lots.html"><?=$categories[$index];?></a>
+            </li>
+            <?php $index++;?>
+        <?php endwhile;?>
+    </ul>
+</section>
+<section class="lots">
+    <div class="lots__header">
+        <h2>Открытые лоты</h2>
+    </div>
+    <ul class="lots__list">
+        <?php $index = 0; ?>
+        <?php while ($index < count($lots_list)): ?>
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src=<?=$lots_list[$index]["image_url"];?> width="350" height="260" alt=<?=$lots_list[$index]["category"];?>>
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?=$lots_list[$index]["category"];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lots_list[$index]["name"];?></a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?=price_format($lots_list[$index]["price"]);?></span>
+                        </div>
+                        <div class="lot__timer timer">
+
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <?php $index++; ?>
+        <?php endwhile; ?>
+    </ul>
+</section>
